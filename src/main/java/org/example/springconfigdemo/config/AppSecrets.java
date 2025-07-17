3223956adad1a54306b3c210a94b0837e0fc76cf
shafique <shafiquelhr0@gmail.com>
2025-07-17 15:28:13 +0500

    first commit

---
+++ src/main/java/org/example/springconfigdemo/config/AppSecrets.java
@@ -0,0 +1,4 @@
+package org.example.springconfigdemo.config;
+
+public class AppSecrets {
+}
---
+++ src/test/java/org/example/springconfigdemo/SpringConfigDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springconfigdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

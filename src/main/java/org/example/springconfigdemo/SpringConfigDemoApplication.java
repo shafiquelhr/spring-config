bdccdf6da15475331dde15429f2507a14e1c224b
shafique <shafiquelhr0@gmail.com>
2025-07-17 15:29:34 +0500

    Refactor

--- src/main/java/org/example/springconfigdemo/SpringConfigDemoApplication.java
+++ src/main/java/org/example/springconfigdemo/SpringConfigDemoApplication.java
@@ -1,13 +1,19 @@
 package org.example.springconfigdemo;
 
+import org.example.springconfigdemo.config.AppSecrets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConfigDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringConfigDemoApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringConfigDemoApplication.class, args);
+
+        // Get the AppSecrets bean
+        AppSecrets secrets = context.getBean(AppSecrets.class);
+        secrets.printSecrets();
     }
 
 }
--- src/main/java/org/example/springconfigdemo/config/AppSecrets.java
+++ src/main/java/org/example/springconfigdemo/config/AppSecrets.java
@@ -1,4 +1,35 @@
 package org.example.springconfigdemo.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AppSecrets {
+
+    @Value("${secret.username}")
+    private String username;
+
+    @Value("${secret.password}")
+    private String password;
+
+    @Value("${environment}")
+    private String environment;
+
+    public void printSecrets() {
+        System.out.println("\nUsername: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("Environment: " + environment);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
 }
